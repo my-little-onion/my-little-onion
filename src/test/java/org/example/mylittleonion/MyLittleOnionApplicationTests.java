b97df2c5a37e7c9bfd0541a8262d54efd0a3428a
박우철 <woochul102@gmail.com>
2024-03-11 17:08:25 +0900

    Feat: First Commit
    안녕? ㅎㅇ

---
+++ src/test/java/org/example/mylittleonion/MyLittleOnionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.mylittleonion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyLittleOnionApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
