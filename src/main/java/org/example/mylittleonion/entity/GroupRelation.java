92ca93449058607a92d3a4716412425a31308aed
dla612 <dla612@naver.com>
2024-03-12 14:52:16 +0900

    feat: entity 클래스 생성 및 연관관계 매핑

---
+++ src/main/java/org/example/mylittleonion/entity/GroupRelation.java
@@ -0,0 +1,24 @@
+package org.example.mylittleonion.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class GroupRelation extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "group_id")
+    private Group group;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "onion_category_id")
+    private OnionCategory onionCategory;
+}
