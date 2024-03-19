4eb1f80a0de2898bbe1698b19ccb6fbf230d3973
Asher Seo <beomxtone@gmail.com>
2024-03-19 15:25:12 +0900

    feat: apply PF Stardust S font to global
    re #S10P22A105-44

--- frontend/src/styles/global.ts
+++ frontend/src/styles/global.ts
@@ -1,10 +1,32 @@
 import { css } from '@emotion/react';
 
+import PFStardustBold from '/fonts/PFStardustS-Bold.woff';
+import PFStardust from '/fonts/PFStardustS.woff';
+
 export const globalStyles = css`
   * {
     box-sizing: border-box;
     margin: 0;
     padding: 0;
     line-height: 1;
+    font-family: 'PFStardust';
+  }
+
+  @font-face {
+    font-family: 'PFStardust';
+    src:
+      local('PFStardust'),
+      url(${PFStardust}) format('woff');
+    font-weight: normal;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'PFStardustBold';
+    src:
+      local('PFStardustBold'),
+      url(${PFStardustBold}) format('woff');
+    font-weight: normal;
+    font-style: normal;
   }
 `;
