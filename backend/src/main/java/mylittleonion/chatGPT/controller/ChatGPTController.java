0aeabefa010d329ca1daaa1f0d5e55b6af39d9ca
김지환 <wlghks98k@gmail.com>
2024-03-19 16:53:52 +0900

    S10P22A105-46 feat : ChatGPT API 컨트롤러 추가

---
+++ backend/src/main/java/mylittleonion/chatGPT/controller/ChatGPTController.java
@@ -0,0 +1,32 @@
+package mylittleonion.chatGPT.controller;
+
+import mylittleonion.chatGPT.dto.CompletionRequestDto;
+import mylittleonion.chatGPT.service.ChatGPTService;
+import mylittleonion.common.dto.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * ChatGPT API
+ */
+@RestController
+@RequestMapping(value = "/api/chatgpt")
+public class ChatGPTController {
+
+    private final ChatGPTService chatGPTService;
+
+    public ChatGPTController(ChatGPTService chatGPTService) {
+        this.chatGPTService = chatGPTService;
+    }
+
+    @PostMapping("/prompt")
+    public ResponseEntity<ApiResponse<Map<String, Object>>> selectPrompt(@RequestBody CompletionRequestDto completionRequestDto) {
+        Map<String, Object> result = chatGPTService.prompt(completionRequestDto);
+        return ResponseEntity.ok(ApiResponse.success(result));
+    }
+
+}
