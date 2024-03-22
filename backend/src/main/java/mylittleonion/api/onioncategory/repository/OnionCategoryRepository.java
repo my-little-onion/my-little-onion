84be823b18359a02596fb20ad5bcc44c192baaa9
dla612 <dla612@naver.com>
2024-03-22 16:13:33 +0900

    feat: 양파 생성 API

---
+++ backend/src/main/java/mylittleonion/api/onioncategory/repository/OnionCategoryRepository.java
@@ -0,0 +1,11 @@
+package mylittleonion.api.onioncategory.repository;
+
+import mylittleonion.common.entity.OnionCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OnionCategoryRepository extends JpaRepository<OnionCategory, Long> {
+
+
+}
