62ad97c30bf1524501ed50d32d24dd5401333a80
김지환 <wlghks98k@gmail.com>
2024-03-28 16:22:12 +0900

    feat : Voice 엔티티 진화단계 컬럼 추가

--- backend/src/main/java/mylittleonion/common/entity/Voice.java
+++ backend/src/main/java/mylittleonion/common/entity/Voice.java
@@ -1,6 +1,14 @@
 package mylittleonion.common.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -25,4 +33,13 @@ public class Voice extends BaseEntity {
   @OneToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "group_id")
   private Group group;
+
+  @Column(name = "firstLevel")
+  private String primary;
+
+  @Column(name = "secondLevel")
+  private String secondary;
+
+  @Column(name = "thirdLevel")
+  private String tertiary;
 }
