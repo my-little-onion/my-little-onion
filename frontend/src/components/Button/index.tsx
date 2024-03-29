779abfa430856d22fea3272d84567159638d322c
Asher Seo <beomxtone@gmail.com>
2024-03-29 15:55:24 +0900

    feat: 양파 선택 페이지 개발
    re #S10P22A105-52

--- frontend/src/components/Button/index.tsx
+++ frontend/src/components/Button/index.tsx
@@ -1,6 +1,15 @@
 import styled from '@emotion/styled';
 import { ReactNode } from 'react';
 
+import theme from '@/styles/theme';
+
+const lightFontColors: string[] = [
+  theme.color.black,
+  theme.color.gray,
+  theme.color.blue,
+  theme.color.green,
+];
+
 const ButtonWrapper = styled.div`
   border: none;
   background-color: transparent;
@@ -9,8 +18,14 @@ const ButtonWrapper = styled.div`
 const ButtonContent = styled.button`
   border: none;
   cursor: pointer;
-  border-radius: 5px;
+  border-radius: 24px;
   margin: auto;
+  background-color: ${(prop) => prop.color};
+  color: ${(prop) =>
+    lightFontColors.includes(prop.color ?? 'none')
+      ? theme.color.white
+      : theme.color.black};
+  font-size: ${theme.fontSize.medium};
 `;
 
 const LargeButton = styled(ButtonContent)`
@@ -19,19 +34,22 @@ const LargeButton = styled(ButtonContent)`
 `;
 
 const SmallButton = styled(ButtonContent)`
-  width: 150px;
+  width: auto;
   height: 40px;
+  padding: 0 20px;
 `;
 
 const SvgButton = styled(ButtonContent)`
   border-radius: 0;
   background-color: transparent;
+  margin: 0;
 `;
 
 interface ButtonProps {
   type: 'submit' | 'reset' | 'button';
   children?: ReactNode;
   size?: 'small' | 'large';
+  color?: string;
   disabled?: boolean;
   onClick?: () => void;
   svg?: ReactNode;
@@ -42,6 +60,7 @@ const Button = ({
   disabled,
   onClick,
   size,
+  color,
   type,
   svg,
 }: ButtonProps) => {
@@ -53,7 +72,12 @@ const Button = ({
     return (
       <ButtonWrapper>
         {/* eslint-disable-next-line react/button-has-type */}
-        <SmallButton type={type} disabled={disabled} onClick={onClick}>
+        <SmallButton
+          type={type}
+          color={color}
+          disabled={disabled}
+          onClick={onClick}
+        >
           {children}
         </SmallButton>
       </ButtonWrapper>
@@ -63,7 +87,12 @@ const Button = ({
   return (
     <ButtonWrapper>
       {/* eslint-disable-next-line react/button-has-type */}
-      <LargeButton type={type} disabled={disabled} onClick={onClick}>
+      <LargeButton
+        type={type}
+        color={color}
+        disabled={disabled}
+        onClick={onClick}
+      >
         {children}
       </LargeButton>
     </ButtonWrapper>
