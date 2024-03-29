0d815ca24b0702e1b688f5b99650e546f68704f4
김지환 <wlghks98k@gmail.com>
2024-03-29 17:18:57 +0900

    feat : GPT응답 response 객체 추가

---
+++ backend/src/main/java/mylittleonion/api/voice/dto/ChatGPTResponse.java
@@ -0,0 +1,11 @@
+package mylittleonion.api.voice.dto;
+
+import lombok.Data;
+
+@Data
+public class ChatGPTResponse {
+
+  private String primary;
+  private String secondary;
+  private String tertiary;
+}
