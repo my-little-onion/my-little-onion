ed45746460e95d179ee42eb68f936560c1932f63
Asher Seo <beomxtone@gmail.com>
2024-03-31 20:16:11 +0900

    feat: 양파 진화 애니메이션 구현
    re #S10P22A105-57

---
+++ frontend/src/components/Evolution/index.tsx
@@ -0,0 +1,110 @@
+import './index.scss';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import { onionNameRecord, onionRecord } from '@/utils/onionRecord';
+
+interface EvolutionProps {
+  before: number;
+  after: number;
+}
+
+const Evolution = ({ before, after }: EvolutionProps) => {
+  const [isEvolve, setIsEvolve] = useState<boolean>(false);
+
+  const evolve = () => {
+    if (isEvolve) return;
+
+    const circles = [...document.getElementsByClassName('circle')];
+    const bubbles = [...document.getElementsByClassName('bubble')];
+    const others = document.getElementsByClassName('current');
+
+    for (let i = 0; i < others.length; i++) {
+      const curr = others[i] as HTMLElement;
+      curr.style.visibility = 'hidden';
+    }
+
+    const animationTime = 12;
+
+    setTimeout(
+      () => {
+        for (let i = 0; i < circles.length; i++) {
+          const circle = circles[i] as HTMLElement;
+          circle.style.animation = '';
+        }
+        for (let i = 0; i < bubbles.length; i++) {
+          const bubble = bubbles[i] as HTMLElement;
+          bubble.style.animation = '';
+        }
+        setIsEvolve(true);
+      },
+      500 + animationTime * 1000,
+    );
+
+    for (let i = 0; i < circles.length; i++) {
+      const circle = circles[i] as HTMLElement;
+      circle.style.animation = `tunnel ${animationTime}s linear ${i * 0.1}s`;
+    }
+    for (let i = 0; i < bubbles.length; i++) {
+      const bubble = bubbles[i] as HTMLElement;
+      bubble.style.animation = `bubble .4s reverse ${animationTime - 2 + i * 0.05}s`;
+    }
+
+    const beforeEvolve = document.getElementsByClassName(
+      'before',
+    )[0] as HTMLElement;
+    beforeEvolve.style.animation = `evolve-out ${animationTime}s forwards`;
+
+    const afterEvolve = document.getElementsByClassName(
+      'after',
+    )[0] as HTMLElement;
+    afterEvolve.style.animation = `evolve-in ${animationTime}s forwards`;
+
+    const background = document.getElementsByClassName(
+      'background',
+    )[0] as HTMLElement;
+    background.style.animation = `background-out ${animationTime}s`;
+  };
+
+  return (
+    <section className='background' onClick={evolve} role='presentation'>
+      <div className='circle c1' />
+      <div className='circle c2' />
+      <div className='circle c3' />
+      <div className='circle c4' />
+      <div className='circle c5' />
+      <div className='evolve'>
+        <div className='bubble-wrap'>
+          {Array.from({ length: 20 }, (_, index) => index + 1).map((value) => (
+            <div className='bubble' key={value} />
+          ))}
+        </div>
+        <img
+          className='onion before current'
+          src={`/images/onions/onion-${onionRecord[before]}.png`}
+          alt='before evolve'
+        />
+        <img
+          className='onion after'
+          src={`/images/onions/onion-${onionRecord[after]}.png`}
+          alt='after evolve'
+        />
+      </div>
+      {isEvolve ? (
+        <Link to='/choose'>
+          <div className='message'>
+            축하합니다! {onionNameRecord[before]}는 <br />
+            {onionNameRecord[after]}로 진화했습니다!
+          </div>
+        </Link>
+      ) : (
+        <div className='message'>
+          ...... 오잉!? <br />
+          양파의 상태가......!
+        </div>
+      )}
+    </section>
+  );
+};
+
+export default Evolution;
