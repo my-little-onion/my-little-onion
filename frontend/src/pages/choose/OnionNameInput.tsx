83d1b1a136c1e57360e82c25d0ac591c7194ee8b
Asher Seo <beomxtone@gmail.com>
2024-04-02 17:44:40 +0900

    feat: 양파 이름으로 생성
    re #S10P22A105-63

---
+++ frontend/src/pages/choose/OnionNameInput.tsx
@@ -0,0 +1,67 @@
+import styled from '@emotion/styled';
+import { ChangeEvent, useState } from 'react';
+import { useSetRecoilState } from 'recoil';
+
+import { onionIndexState } from '@/pages/choose/store';
+import { createOnion } from '@/services/onion';
+
+const InputWrapper = styled.div`
+  width: 100%;
+  margin-top: 16px;
+  display: flex;
+  gap: 16px;
+`;
+
+const NameInput = styled.input`
+  height: 28px;
+  outline: none;
+`;
+
+const CreateButton = styled.button`
+  height: 28px;
+  background-color: black;
+  color: white;
+  border: none;
+  border-radius: 2px;
+  padding: 0 8px;
+`;
+
+interface OnionNameInput {
+  indexToSet: number;
+  fetchData: () => void;
+}
+
+const OnionNameInput = ({ indexToSet, fetchData }: OnionNameInput) => {
+  const [onionName, setOnionName] = useState<string>('');
+  const setOnionIndex = useSetRecoilState(onionIndexState);
+
+  const handleCreateClick = async () => {
+    await createOnion(onionName);
+    setOnionIndex(indexToSet);
+    fetchData();
+  };
+
+  const handleNameInput = (event: ChangeEvent<HTMLInputElement>) => {
+    console.log(event.target.value);
+    if (event.target.value.length > 15) {
+      event.target.value = event.target.value.slice(0, 15);
+    }
+    setOnionName(event.target.value);
+  };
+
+  return (
+    <InputWrapper>
+      <NameInput
+        type='text'
+        onChange={handleNameInput}
+        value={onionName}
+        placeholder='15자 이내로 말해줘'
+      />
+      <CreateButton type='button' onClick={handleCreateClick}>
+        생성
+      </CreateButton>
+    </InputWrapper>
+  );
+};
+
+export default OnionNameInput;
